3be105db90da1038ae20c563f63a60670249b2f7
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2023-11-03 17:54:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleDarkModeButton = document.getElementById("toggleDarkMode");
-    const body = document.body;
-
-    toggleDarkModeButton.addEventListener("click", function () {
-        body.classList.toggle("dark-theme");
-    });
-});
