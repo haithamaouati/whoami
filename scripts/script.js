864479dff6d82589104a4a8c6cc1168b8664bb9d
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-12-14 22:30:18 +0100

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+// ASCII Art on Console
+console.log(`
+  _  __  _   _     _           
+ | |/ / (_) | |_  | |_   _   _ 
+ | ' /  | | | __| | __| | | | |
+ | . \  | | | |_  | |_  | |_| |
+ |_|\_\ |_|  \__|  \__|  \__, |
+                         |___/ 
+`);
+console.log("%c I love ♡ Kitty 👀", "color: green; font-size: 16px; font-weight: bold;");
