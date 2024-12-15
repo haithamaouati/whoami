03581cbf78853eaeb5d6f9c6c9cfbda1d4fe9ea7
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-12-15 13:45:19 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,2 @@
-// Define the CSS styles for console.log
-const authorStyle = "color: green; font-size: 16px; font-weight: bold;";
-const githubStyle = "color: blue; font-size: 14px; text-decoration: underline;";
-
-// Log the styled messages to the console
-console.log("%cAuthor: Haitham Aouati", authorStyle);
-console.log("%cGitHub: https://github.com/haithamaouati", githubStyle);
+console.log("Author: Haitham Aouati");
+console.log("GitHub: https://github.com/haithamaouati");
