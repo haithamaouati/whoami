9fa79ddaffbe03d55180455fc3ec1a0b62be8e25
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-12-15 15:54:30 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,2 @@
-console.log(`%c
-  _   _                _    _   _    _
- | | | | __ ___      _| | _| | | |  / \
- | |_| |/ _` \ \ /\ / / |/ / |_| | / _ \
- |  _  | (_| |\ V  V /|   <|  _  |/ ___ \
- |_| |_|\__,_| \_/\_/ |_|\_\_| |_/_/   \_\
-`, `font-family: monospace`);
 console.log("Author: Haitham Aouati");
 console.log("GitHub: https://github.com/haithamaouati");
